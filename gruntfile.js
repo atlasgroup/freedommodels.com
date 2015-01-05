e70bd8b0c3cf24f0f09b5f6f3188533acda61700
R.V.d.M <r@rvdm.la>
2015-01-05 13:26:27 -0800

    First commit

---
+++ gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    aws: grunt.file.readJSON( '../../Amazon/freedom@agencycloud.io/rvdm.json' ),
+
+    s3: {
+
+      options: {
+
+        accessKeyId: "<%= aws.accessKeyId %>",
+        secretAccessKey: "<%= aws.secretAccessKey %>",
+        bucket: "freedommodels",
+        region: "us-west-1",
+        headers: {
+
+          CacheControl: 60
+
+        }
+
+      },
+
+      public : {
+
+        cwd : 'public/',
+        src : '**'
+
+      }
+
+    }
+
+  });
+
+  grunt.loadNpmTasks( 'grunt-aws' );
+
+  grunt.registerTask( 'default' , [ 's3' ]);
+
+};
